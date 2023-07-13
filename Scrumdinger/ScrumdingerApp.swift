547ff3804ba0db45716448fd4361d620d455dddb
Shreyansh Dixit <shreyansh.dixit@gmail.com>
2023-07-12 23:54:47 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Shreyansh Dixit on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
