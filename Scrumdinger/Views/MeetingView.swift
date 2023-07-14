b4ba0994aaa463c2db5be2fdaaf8d370b3efa7bf
Shreyansh Dixit <shreyansh.dixit@gmail.com>
2023-07-13 23:19:44 -0500

    Card Views created... earlier background color issue still persists for whatever reason.

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -11,29 +11,32 @@ struct MeetingView: View {
     var body: some View {
         VStack {
             ProgressView(value: 5, total: 15)
-            HStack{
-                VStack(alignment: .leading){
-                    Text("Seconds Elapsed").font(.caption)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.tophalf.fill")
                 }
                 Spacer()
-                VStack(alignment: .trailing){
-                    Text("Seconds Remaining").font(.caption)
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
             .accessibilityElement(children: .ignore)
             .accessibilityLabel("Time remaining")
             .accessibilityValue("10 minutes")
-            Circle().strokeBorder(lineWidth: 24)
-            HStack{
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
                 Text("Speaker 1 of 3")
                 Spacer()
-                Button(action: {}){
+                Button(action: {}) {
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel("Next speaker")
             }
-            .accessibilityLabel("Next speaker")
         }
         .padding()
     }
