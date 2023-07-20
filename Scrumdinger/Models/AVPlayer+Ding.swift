a4bb69877755f1ba07a44505cd65bbda0d34989e
Shreyansh Dixit <shreyansh.dixit@gmail.com>
2023-07-20 13:32:53 -0500

    managing state and life cycle - starter files added

---
+++ Scrumdinger/Models/AVPlayer+Ding.swift
@@ -0,0 +1,13 @@
+/*
+starter file(s) provided by Apple
+*/
+
+import Foundation
+import AVFoundation
+
+extension AVPlayer {
+    static let sharedDingPlayer: AVPlayer = {
+        guard let url = Bundle.main.url(forResource: "ding", withExtension: "wav") else { fatalError("Failed to find sound file.") }
+        return AVPlayer(url: url)
+    }()
+}
