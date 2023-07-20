14e07cb298526429918df3b98b286ec54f279298
Shreyansh Dixit <shreyansh.dixit@gmail.com>
2023-07-20 17:18:58 -0500

    created a data store

---
+++ Scrumdinger/Models/ScrumStore.swift
@@ -0,0 +1,20 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by Shreyansh Dixit on 7/20/23.
+//
+
+import Foundation
+
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("scrums.data")
+    }
+}
